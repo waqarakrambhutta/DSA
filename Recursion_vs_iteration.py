a825a88fd346d9b401ee23589004075b95416679
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-09-28 11:00:39 +0500

    Recursion: recursion vs iteration.

--- Recursion_vs_iteration.py
+++ Recursion_vs_iteration.py
@@ -10,4 +10,17 @@ def powerOfTwo(n):
 
     
 # print(powerOfTwo(3))
-powerOfTwo(5)
+
+#Iteration:
+def powerOfTwoIt(n):
+    power = 1
+    i = 0
+    while i<n:
+        power= power*2
+        i = i+1
+    return power
+
+print(powerOfTwoIt(3))
+
+
+        
