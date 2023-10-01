0a5971861cdfc76cdad3521b49bf752e69e382c7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-01 17:16:12 +0500

    Recursion: unintentional and constraints in the assert statment

--- Recursion_fibonacci.py
+++ Recursion_fibonacci.py
@@ -1,7 +1,8 @@
 def fibonacci(n):
+    assert n>=0 and n == int(n), 'The number must be a positive integer.'
     if n in [0,1]:
         return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-print(fibonacci(-1))
+print(fibonacci(10))
