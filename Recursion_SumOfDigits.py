55fca9ba68d2cabf65955e0b808b366b4f002be1
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-01 17:49:38 +0500

    Recursion: how to find sum of digits of a positive integer??

---
+++ Recursion_SumOfDigits.py
@@ -0,0 +1,8 @@
+def SumOfDigits(n):
+    assert n>=0 and n == int(n),'The number must be a positive integer.'
+    if n==0:
+        return n
+    else:
+        return int(n%10) + SumOfDigits(int(n/10))
+
+print(SumOfDigits(12234))
