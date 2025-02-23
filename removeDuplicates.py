f75bfbd1e918193e77ecc0b90e222e7cbd1345e3
ammakr <aakrambhutta@gmail.com>
2025-02-23 16:03:43 +0500

    Failed

---
+++ removeDuplicates.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        counter = 0 
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+        return j  
+
+instance = Solution().removeDuplicates([1,1,1,1])
+print(instance)
